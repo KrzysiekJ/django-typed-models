1bee786dad870cef042e7cf64d3a316930be048c
Krzysztof Jurewicz <krzysztof.jurewicz@laboratorium.ee>
2012-11-27 17:51:52 +0100

    Admin bugfixes and not displaying changelist link to the same view.

--- typedmodels/templatetags/typedmodels_admin_tags.py
+++ typedmodels/templatetags/typedmodels_admin_tags.py
@@ -1,10 +1,14 @@
 from django import template
 
 register = template.Library()
-@register.inclusion_tag("typedmodels/admin/_links_for_model_with_children.html")
-def links_for_model_with_children(model):
+@register.inclusion_tag("typedmodels/admin/_links_for_model_with_children.html", takes_context=True)
+def links_for_model_with_children(context, model):
     # See https://code.djangoproject.com/ticket/3544 for the reason why template recursion is not used here.
-    return {'model': model}
+    if "cl" in context:
+        current_model = context["cl"].model
+    else:
+        current_model = context["current_model"]
+    return {'model': model, 'current_model': current_model}
 
 @register.filter
 def typedmodel_ancestors(model):
